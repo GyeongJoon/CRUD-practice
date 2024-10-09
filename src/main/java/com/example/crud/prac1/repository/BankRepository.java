462372f9119053032ba16540bd274984df76aac7
GyeongJoon <jjangjoon328@naver.com>
2024-10-09 17:44:32 +0900

    [fix] Repository 생성

--- src/main/java/com/example/crud/prac1/repository/BankRepository.java
+++ src/main/java/com/example/crud/prac1/repository/BankRepository.java
@@ -1,7 +1,14 @@
 package com.example.crud.prac1.repository;
 
 import com.example.crud.prac1.domain.Bank;
+import com.example.crud.prac1.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
+    Optional<Bank> findByBankNameAndCustomer(String bankName, Customer customer);
 }
