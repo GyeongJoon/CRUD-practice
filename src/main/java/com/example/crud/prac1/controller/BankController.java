755ac26f49217747d552215955b5b1a4ba9b1829
GyeongJoon <jjangjoon328@naver.com>
2024-10-14 12:54:11 +0900

    [fix] controller 수정

--- src/main/java/com/example/crud/prac1/controller/BankController.java
+++ src/main/java/com/example/crud/prac1/controller/BankController.java
@@ -1,4 +1,41 @@
 package com.example.crud.prac1.controller;
 
+import com.example.crud.prac1.dto.BankRequestDto;
+import com.example.crud.prac1.dto.BankResponseDto;
+import com.example.crud.prac1.dto.LoanResponseDto;
+import com.example.crud.prac1.service.BankService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("customer/{customerId}/bank")
 public class BankController {
+
+    private final BankService bankService;
+
+    // 은행 등록
+    @PostMapping
+    public ResponseEntity<String> createBank(@PathVariable Long customerId, @RequestBody BankRequestDto bankRequestDto) {
+        ResponseEntity<String> response = bankService.createBank(customerId, bankRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response.getBody());
+    }
+
+    // 은핻 조회
+    @GetMapping("/{bankId}/name")
+    public ResponseEntity<BankResponseDto> getBankById(@PathVariable Long customerId, @PathVariable Long bankId) {
+        BankResponseDto bankResponseDto = bankService.getBankById(customerId, bankId);
+        return ResponseEntity.ok(bankResponseDto);
+    }
+
+    // 대출 조회
+    @GetMapping("/{bankId}/loan")
+    public ResponseEntity<LoanResponseDto> getLoanById(@PathVariable Long customerId, @PathVariable Long bankId) {
+        LoanResponseDto bankResponseDto = bankService.getLoanById(customerId, bankId);
+        return ResponseEntity.ok(bankResponseDto);
+    }
 }
--- src/main/java/com/example/crud/prac1/controller/CustomerController.java
+++ src/main/java/com/example/crud/prac1/controller/CustomerController.java
@@ -1,4 +1,33 @@
 package com.example.crud.prac1.controller;
 
+import com.example.crud.prac1.dto.CustomerRequestDto;
+import com.example.crud.prac1.dto.CustomerResponseDto;
+import com.example.crud.prac1.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    // 유저 등록
+    @PostMapping
+    public ResponseEntity<String> creteCustomer(@RequestBody CustomerRequestDto customerRequestDto) {
+        ResponseEntity<String> response = customerService.createCustomer(customerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response.getBody());
+    }
+
+    // 유저 조회
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDto> getCustomerById(@PathVariable Long customerId) {
+        CustomerResponseDto customerResponseDto = customerService.getCustomerById(customerId);
+        return ResponseEntity.ok(customerResponseDto);
+    }
 }
