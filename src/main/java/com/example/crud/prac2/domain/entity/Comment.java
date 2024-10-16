e68a8f6595cd4a0173a89d0df6ba2cd5eac2c182
GyeongJoon <jjangjoon328@naver.com>
2024-10-16 18:50:51 +0900

    [feat] Entity 생성

---
+++ src/main/java/com/example/crud/prac2/domain/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.example.crud.prac2.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Comment extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
